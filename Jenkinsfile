f6c2dfb7fee29356334ad8fcf04f114955763468
raphealolams <raphealolams@gmail.com>
2020-01-28 17:54:09 +0100

    network and server file added

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
     }
     agent any
     stages {
-        stage('Lint HTML') {
+        stage('Lint Vue') {
             steps {
                 sh 'tidy -q -e *.html'
             }
---
+++ create_infra.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+UUID="$(uuidgen)"
+
+aws cloudformation create-stack --stack-name capstone_project$UUID --template-body file://capstone_infra.yml \
+--parameters file://capstone_infra_parameter.json --region=eu-west-1
