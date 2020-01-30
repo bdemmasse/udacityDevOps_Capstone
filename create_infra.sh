9d70d46f1470533ce33ad1c17a3a5db71f231285
raphealolams <raphealolams@gmail.com>
2020-01-30 13:58:44 +0100

    echo

--- create_infra.sh
+++ create_infra.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-UUID="$(uuidgen)"
-
-aws cloudformation create-stack --stack-name capstone_project$UUID --template-body file://server.yml \
+aws cloudformation create-stack --stack-name capstone_project --template-body file://server.yml \
 --parameters file://server-parameters.json --region=eu-west-1
